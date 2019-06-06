04f72aa688b9a9dc9cfbe39959e59691ed824579
ErikMartensson <erik@acid.nu>
2019-06-06 16:23:44 +0200

    Added check/uncheck endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateItemDto, ItemDto } from './dtos/item.dto';
 import { Item } from './entities/item.entity';
@@ -10,6 +10,18 @@ export class AppController {
   @Post()
   async postItem(@Body() createData: CreateItemDto): Promise<ItemDto> {
     const item = await this.appService.createItem(createData.content);
+    return item.toDto();
+  }
+
+  @Put(':id/check')
+  async checkItem(@Param('id') id: string): Promise<ItemDto> {
+    const item = await this.appService.checkItem(id);
+    return item.toDto();
+  }
+
+  @Put(':id/uncheck')
+  async uncheckItem(@Param('id') id: string): Promise<ItemDto> {
+    const item = await this.appService.uncheckItem(id);
     return item.toDto();
   }
 }
