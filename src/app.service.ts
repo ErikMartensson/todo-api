13ac34b38c800dbb06accc3c754198b6354f99d3
ErikMartensson <ErikMartensson@users.noreply.github.com>
2019-06-06 16:23:44 +0200

    Added check/uncheck endpoints

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { ItemRepository} from './repositories/item.repository';
 import { Item } from './entities/item.entity';
 
@@ -9,5 +9,23 @@ export class AppService {
   async createItem(content: string): Promise<Item> {
     const item = this.itemRepository.create({ content })
     return this.itemRepository.save(item)
+  }
+
+  async checkItem(id: string): Promise<Item> {
+    const item = await this.itemRepository.findOne(id);
+    if (!item) {
+      throw new HttpException('Item not found', HttpStatus.NOT_FOUND);
+    }
+    item.isChecked = true;
+    return this.itemRepository.save(item);
+  }
+
+  async uncheckItem(id: string): Promise<Item> {
+    const item = await this.itemRepository.findOne(id);
+    if (!item) {
+      throw new HttpException('Item not found', HttpStatus.NOT_FOUND);
+    }
+    item.isChecked = false;
+    return this.itemRepository.save(item);
   }
 }
